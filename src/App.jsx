7a56f05e07bf64eb96e8a488e19543e74aaeaf32
mrinal1224 <iammrinalkc@gmail.com>
2021-06-26 03:00:40 +0530

    Interface created

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './app.css'
+
+
+function App() {
+  return (
+    
+      <div className='app'>
+        <div className="main">main</div>
+        <div className="pyramid">amount</div>
+
+      </div>
+    
+  );
+}
+
+export default App;
