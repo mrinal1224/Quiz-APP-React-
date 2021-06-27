6b83a97a2c779c0a3164e93d6b6507f27fbb2fa4
mrinal1224 <iammrinalkc@gmail.com>
2021-06-28 02:27:26 +0530

    amountList created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,43 @@
 
+import { useState } from 'react';
 import './app.css'
 
 
 function App() {
+  const quesArray=[
+    {id:1 , amount:1000} , 
+    {id:2 , amount:2000},
+    {id:3 , amount:5000},
+    {id:4 , amount:10000},
+    {id:5 , amount:25000},
+    {id:6 , amount:50000},
+    {id:7 , amount:100000},
+    {id:8, amount:200000},
+    {id:9, amount:200000},
+    {id:10, amount:200000},
+  ]
+
+
+  const[questionNumber , setQuestionNumber] = useState(1)
   return (
     
       <div className='app'>
         <div className="main">main</div>
         <div className="pyramid">
           <ul className='moneylist'>
-            <li>300</li>
-             <li>200</li>
-              <li>100</li>
+            {
+              quesArray.map((ques) =>(
+                  <li className={questionNumber=== ques.id ? "moneylistItem active" :'moneylistItem'}>
+                <span className='moneylistnumber'>{ques.id}</span> <span className='moneylistamount'>Rs.{ques.amount}</span>
+               </li>
+
+              )).reverse()
+            }
+          
+             
+            
+          
+             
               
           </ul>
         </div>
