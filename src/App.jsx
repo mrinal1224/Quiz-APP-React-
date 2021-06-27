124c7037e8caebe8eb3e6ec0e5e7531c241b6d77
mrinal1224 <iammrinalkc@gmail.com>
2021-06-27 23:10:00 +0530

    moneylist

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
     
       <div className='app'>
         <div className="main">main</div>
-        <div className="pyramid">amount</div>
+        <div className="pyramid">
+          <ul className='moneylist'>
+            <li>300</li>
+             <li>200</li>
+              <li>100</li>
+              
+          </ul>
+        </div>
 
       </div>
     
